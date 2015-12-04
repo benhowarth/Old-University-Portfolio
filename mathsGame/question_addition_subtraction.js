c729f9b441bb0fa968afda5ae8ca1b509c2fe416
benhowarth <benhowarthmail@gmail.com>
2015-12-04 13:25:09 +0000

    0.1.8.7
    Added fake answers for multiplication and additionsubtraction

--- mathsGame/question_addition_subtraction.js
+++ mathsGame/question_addition_subtraction.js
@@ -2,14 +2,18 @@ function adSubGen(){
 	x=Math.floor((Math.random())*50)+1;
 	y=Math.floor((Math.random())*50)+1;
 	var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+	var plusOrMinus2 = Math.random() < 0.5 ? -1 : 1;
+	fakeAns1=Math.floor((Math.random())*4)+1;
+	var plusOrMinus3 = Math.random() < 0.5 ? -1 : 1;
+	fakeAns2=Math.floor((Math.random())*2)+1;
 
 	if(plusOrMinus<0){plusOrMinusStr="-"}
 	else{plusOrMinusStr="+"}
 	
 	adSubQuestion=x+plusOrMinusStr+y;
 	correctAnswer=x+(plusOrMinus*y);
-	wrongAnswer1="wrong1";
-	wrongAnswer2="wrong2";
+	wrongAnswer1=x+(plusOrMinus*y)+(plusOrMinus2*fakeAns1);
+	wrongAnswer2=x+(plusOrMinus*y)+(plusOrMinus3*fakeAns2);
 	problem=[adSubQuestion,[correctAnswer,wrongAnswer1,wrongAnswer2],0];
 	problem[1]=$.shuffle(problem[1]);
 	problem[2]=problem[1].indexOf(correctAnswer);
--- mathsGame/question_multiplication.js
+++ mathsGame/question_multiplication.js
@@ -2,10 +2,13 @@ function multiplyGen(){
 	x=Math.floor((Math.random())*100)+1;
 	y=Math.floor((Math.random())*100)+1;
 	
+	y1=Math.floor((Math.random())*8)-4;
+	y2=Math.floor((Math.random())*8)-4;
+	
 	adSubQuestion=x+"X"+y;
 	correctAnswer=x*y;
-	wrongAnswer1="wrong1";
-	wrongAnswer2="wrong2";
+	wrongAnswer1=x*(y+y1);
+	wrongAnswer2=x*(y+y2);
 	problem=[adSubQuestion,[correctAnswer,wrongAnswer1,wrongAnswer2],0];
 	problem[1]=$.shuffle(problem[1]);
 	problem[2]=problem[1].indexOf(correctAnswer);
